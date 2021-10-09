fa038173a48b8eda21aa4f1a27c6b86a3af2a9a1
Cas van Luijtelaar <casvanluijtelaar@hotmail.com>
2021-10-09 15:33:40 +0200

    added color camp assertions

--- lib/core/utils/color_extensions.dart
+++ lib/core/utils/color_extensions.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/painting.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 
 extension ColorExtension on Color {
   /// darkens the color by an [amount] where 1.0 is min brightness and 0.0 is
@@ -8,7 +7,9 @@ extension ColorExtension on Color {
     assert(amount >= 0 && amount <= 1);
 
     final hsl = HSLColor.fromColor(this);
-    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+    final hslDark = hsl.withLightness(
+      (hsl.lightness - amount).clamp(0.0, 1.0),
+    );
 
     return hslDark.toColor();
   }
@@ -19,20 +20,24 @@ extension ColorExtension on Color {
     assert(amount >= 0 && amount <= 1);
 
     final hsl = HSLColor.fromColor(this);
-    final hslLight =
-        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+    final hslLight = hsl.withLightness(
+      (hsl.lightness + amount).clamp(0.0, 1.0),
+    );
 
     return hslLight.toColor();
   }
 
-  /// clamps a color's rgb values between a [min] and [max]
+  /// clamps a color's brightness values between a [min] and [max] were both
+  /// range between 0 and 1
   Color clamp(double min, double max) {
     assert(min >= 0 && min <= 1);
     assert(max >= 0 && max <= 1);
+    assert(min < max);
 
     final l = computeLuminance();
     if (l < min) return lighten(min - l);
     if (l > max) return darken(l - max);
+
     return this;
   }
 }
